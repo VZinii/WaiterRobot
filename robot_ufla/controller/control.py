51727b6ec926683bc138464b6e30f009c001c10f
thiagof201014 <63051022+thiagof201014@users.noreply.github.com>
2023-09-13 14:09:32 -0300

    Falas adicionadas(Necessario teste)

--- robot_ufla/controller/control.py
+++ robot_ufla/controller/control.py
@@ -3,15 +3,25 @@ import json
 import random
 
 def runRobot():
-  stt = sense.speech_to_text()
-  print('STT = ', stt)
+  # O robo pergunta qual o pedido
+  act.act("Informe qual o seu pedido.") 
+  pedido = sense.speech_to_text()
+  print('Pedido = ', pedido)
+  
+  # Agora o robo da meia volta e anda ate o cozinheiro
+  
+  # O robo informa qual o pedido para o cozinheiro
+  act.act("O pedido é: ", pedido)
+  
+  # O robo da meia volta e retorna ao cliente
 
-  with open ('./model/dataset.json', 'r') as file:
-    dataset = json.loads(file.read())
-    for word in stt:
-      if word in dataset['cumprimentos']:
-        act.act(random.choice(dataset['resCumprimentos']))
-        break
-      elif word in dataset['despedidas']:
-        act.act(random.choice(dataset['resDespedidas']))
-        break
+  #
+  #with open ('./model/dataset.json', 'r') as file:
+  #  dataset = json.loads(file.read())
+  #  for word in stt:
+  #    if word in dataset['cardapio']:
+  #      act.act(random.choice(dataset['resCumprimentos']))
+  #      break
+  #    elif word in dataset['despedidas']:
+  #      act.act(random.choice(dataset['resDespedidas']))
+  #      break
