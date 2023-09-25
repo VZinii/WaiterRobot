217f2a210f1262b6b28667e7d3e3563683a8d5c3
danielSantos4 <102684238+danielSantos4@users.noreply.github.com>
2023-09-25 11:05:00 -0300

    Update control.py

--- robot_ufla/controller/control.py
+++ robot_ufla/controller/control.py
@@ -4,24 +4,36 @@ import random
 
 def runRobot():
   # O robo pergunta qual o pedido
-  act.act("Informe qual o seu pedido.") 
+  act.speak("Bom dia. Posso anotar seu pedido?") 
   pedido = sense.speech_to_text()
-  print('Pedido = ', pedido)
+  escolhas = []
+
+  while "não" not in pedido:
+    escolhas.append(pedido)
+    print('Pedido = ', pedido)
+    act.speak("Mais alguma coisa?")
+    pedido = sense.speech_to_text()
+  act.speak("Vou levar seu pedido para a cozinha.")
   
   # Agora o robo da meia volta e anda ate o cozinheiro
   
   # O robo informa qual o pedido para o cozinheiro
-  act.act("O pedido é: ", pedido)
-  
-  # O robo da meia volta e retorna ao cliente
+  if escolhas != []:
+    act.speak(f"O cliente pediu os itens: {escolhas}. Quando eu puder voltar com os pedidos diga, pronto.")
 
-  #
-  #with open ('./model/dataset.json', 'r') as file:
-  #  dataset = json.loads(file.read())
-  #  for word in stt:
-  #    if word in dataset['cardapio']:
-  #      act.act(random.choice(dataset['resCumprimentos']))
-  #      break
-  #    elif word in dataset['despedidas']:
-  #      act.act(random.choice(dataset['resDespedidas']))
-  #      break
+  res = []
+  while res == [] or res[0] != "pronto":
+    res = sense.speech_to_text()
+
+  act.speak("Voltando para entregar o pedido ao cliente.")
+  # O robo da meia volta e retorna ao cliente
+  
+
+  # Robo chega no cliente e pede para que peguem os pedidos
+  act.speak("Aqui está seu pedido. Peço que retire os pedidos e diga pronto quando tudo tiver sido retirado.")
+  res = []
+  res = sense.speech_to_text()
+  while res[0] != "pronto":
+    res = sense.speech_to_text()
+    print(res)
+  act.speak("Bom apetite.")
