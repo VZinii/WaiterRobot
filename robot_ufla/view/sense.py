43eaceff3b9fc611a673c00f986165d02aa8985a
danielSantos4 <102684238+danielSantos4@users.noreply.github.com>
2023-09-25 11:06:34 -0300

    Update sense.py

--- robot_ufla/view/sense.py
+++ robot_ufla/view/sense.py
@@ -6,13 +6,13 @@ def speech_to_text():
 
   # Capture audio from the microphone
   with sr.Microphone() as source:
-    # print(sr.Microphone.list_microphone_names())
+    print(sr.Microphone.list_microphone_names())
     print("Fale algo...")
     recognizer.adjust_for_ambient_noise(source)  # Adjust for noise
     audio = recognizer.listen(source)
-
+  
   try:
-    # Recognize the speech using Google Web Speech API
+    # Recognize the speech using Google Web Speech API  
     text = recognizer.recognize_google(audio, language='pt-BR')
     text = text.lower()
     stt = text.split(' ')
@@ -20,5 +20,6 @@ def speech_to_text():
     # print("Você falou:", stt)
   except sr.UnknownValueError:
     print("Sorry, I couldn't understand the audio.")
+    return []
   except sr.RequestError as e:
     print("Sorry, an error occurred. Could not request results; {0}".format(e))
