5c87d8507651e6daacdd574c14313379639cf9f3
danielSantos4 <102684238+danielSantos4@users.noreply.github.com>
2023-09-25 11:05:58 -0300

    Update act.py

--- robot_ufla/view/act.py
+++ robot_ufla/view/act.py
@@ -1,16 +1,33 @@
-# import pyttsx3
+import pyttsx3
 import time
 
-from elevenlabs import generate, play
+from gtts import gTTS
+import os
 
-
-
-def act(str):
+# def act(str):
+    # str = "Pessoa falante aqui"
     # engine = pyttsx3.init()
     # engine.say(str)
     # time.sleep(3)
     # engine.runAndWait()
     # time.sleep(3)
-    audio = generate(text=str, voice="Daniel", model="eleven_multilingual_v2")
+    # audio = gTTS(text=str, lang="pt", slow=False)
+    # audio.save("example.mp3")
+    # os.system("start example.mp3")
+
+    # play(audio)
+    # time.sleep(3)
+
+from elevenlabs import clone, generate, play, set_api_key
+from elevenlabs.api import History
+
+set_api_key("c7f8ff1ea7527c73e4015f9f9e1917ff")
+
+def speak(str):
+    audio = generate(
+        text=str,
+        voice="Daniel",
+        model="eleven_multilingual_v2"
+    )
+
     play(audio)
-    time.sleep(3)
