d0c8dd11b0d837ca9c039d05b012331e77bde788
VZinii <108377833+VZinii@users.noreply.github.com>
2023-09-26 14:34:11 -0300

    Funções do Robô atualizadas

--- robot_ufla/controller/control.py
+++ robot_ufla/controller/control.py
@@ -1,9 +1,12 @@
 from view import sense, act
 import json
 import random
+import time
+import serial
 
 def runRobot():
   # O robo pergunta qual o pedido
+
   act.speak("Bom dia. Posso anotar seu pedido?") 
   pedido = sense.speech_to_text()
   escolhas = []
@@ -15,25 +18,27 @@ def runRobot():
     pedido = sense.speech_to_text()
   act.speak("Vou levar seu pedido para a cozinha.")
   
-  # Agora o robo da meia volta e anda ate o cozinheiro
-  
+  # Agora o robo vai até a cozinha
+  act.walkFront()
+
   # O robo informa qual o pedido para o cozinheiro
   if escolhas != []:
     act.speak(f"O cliente pediu os itens: {escolhas}. Quando eu puder voltar com os pedidos diga, pronto.")
 
   res = []
-  while res == [] or res[0] != "pronto":
+  while "pronto" not in res:
     res = sense.speech_to_text()
 
   act.speak("Voltando para entregar o pedido ao cliente.")
   # O robo da meia volta e retorna ao cliente
-  
+  act.turnaround()
+  act.walkFront()
 
   # Robo chega no cliente e pede para que peguem os pedidos
   act.speak("Aqui está seu pedido. Peço que retire os pedidos e diga pronto quando tudo tiver sido retirado.")
   res = []
   res = sense.speech_to_text()
-  while res[0] != "pronto":
+  while "pronto" not in res:
     res = sense.speech_to_text()
     print(res)
   act.speak("Bom apetite.")
--- robot_ufla/view/act.py
+++ robot_ufla/view/act.py
@@ -1,5 +1,7 @@
 import pyttsx3
 import time
+import serial
+import msvcrt
 
 from gtts import gTTS
 import os
@@ -31,3 +33,70 @@ def speak(str):
     )
 
     play(audio)
+
+def walkFront():
+
+    serial_port = 'COM3'  # Change this to your actual Bluetooth serial port
+    baud_rate = 9600
+
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    def send_command(command):
+        ser.write(command.encode())
+        time.sleep(0.3)  # Add a delay after sending each command
+
+    key_last_sent_time = 0  # Initialize variable to track the last time a key was sent
+    key_delay = 0.5  # Set the time delay in seconds between sending keys
+
+    send_command('F')
+    time.sleep(3.5)
+    # send_command('B')
+    # time.sleep(3)
+    send_command('L')
+    time.sleep(0.15)
+    send_command('F')
+    time.sleep(4)
+    send_command('S')
+    time.sleep(1)
+
+
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+
+
+    ser.close()
+    time.sleep(3)
+
+def turnaround():
+
+    serial_port = 'COM3'  # Change this to your actual Bluetooth serial port
+    baud_rate = 9600
+
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    def send_command(command):
+        ser.write(command.encode())
+        time.sleep(0.3)  # Add a delay after sending each command
+
+    key_last_sent_time = 0  # Initialize variable to track the last time a key was sent
+    key_delay = 0.5  # Set the time delay in seconds between sending keys
+
+    send_command('L')
+    time.sleep(1.72)
+    send_command('S')
+
+
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+# send_command('L')
+
+
+    ser.close()
+    time.sleep(3)
